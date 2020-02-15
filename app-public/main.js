48101d573a9a7d1181b18eb126d1967009376554
navidmx <navidmx@gmail.com>
2020-02-15 01:55:13 -0500

    Buttons

--- app-public/main.js
+++ app-public/main.js
@@ -1,11 +1,17 @@
-$('#start').click(function() {
-    $.post('/api', { state: 'start' }).done(function(data) {
-        console.log('Data Loaded: ' + data);
-    });
-});
+const startButton = () => {
+    $('button').each((i, item) => {(item.id == 'start') ? $(item).hide() : $(item).show();});
+}
 
-$('#stop').click(function() {
-    $.post('/api', { state: 'stop' }).done(function(data) {
-        console.log('Data Loaded: ' + data);
+const restartButton = () => {
+    $('button').each((i, item) => {(item.id == 'start') ? $(item).show() : $(item).hide();});
+}
+
+$('button').each((i, item) => {
+    $(item).click(() => {
+        if (item.id == 'start') startButton();
+        else if (item.id == 'lobby') restartButton();
+        $.post('/api', { state: item.id }).done((data) => {
+            console.log('Data Loaded: ' + data);
+        });
     });
 });
