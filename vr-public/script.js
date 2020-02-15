d945b2a7939f9a7270df83136323ac51c1157682
Gabriel Rasskin <43894452+grasskin@users.noreply.github.com>
2020-02-15 16:09:33 -0500

    small video

--- vr-public/script.js
+++ vr-public/script.js
@@ -120,57 +120,57 @@ let tooltips = {
     skywards_miles: {
         text:
             'Earning Miles quickly is naturally one of the things our frequent flyers really care about. Earn both Tier Miles and Skywards Miles on Emirates and flydubai flights, even on special offers. You can also earn Skywards Miles on flights with our growing alliance of partner airlines, and with exclusive offers from our global partners in hospitality, car hire, finance, lifestyle and retail. The best part about earning Miles is deciding how to spend them, and it won’t take you long to reach exciting rewards.',
-        position: '-0.4 1.5 -2',
+        position: '0 1.5 -1',
         enabled: false,
-        color: '#FF2400'
+        color: 'FF2400'
     },
     skywards_membership: {
         text:
             'There’s plenty to look forward to as an Emirates Skywards member when you fly with Emirates and flydubai. As soon as you join, you can take advantage of our member services to streamline planning and enjoy a seamless journey. As you move up tiers, you’ll find more and more to enjoy about every trip, from lounge access at the airport, to instant upgrades on board and priority luggage delivery at your destination.',
-        position: '-0.2 1.5 -2',
+        position: '0 1.5 -1',
         enabled: false,
-        color: '#B22222'
+        color: 'B22222'
     },
     skywards_family: {
         text:
             'Whenever you fly with Emirates or flydubai you can choose to contribute some or all of the Skywards Miles you earn into your My Family account. Any remaining Miles will go into your individual Emirates Skywards account.',
-        position: '-0.6 1.5 -2',
+        position: '0 1.5 -1',
         enabled: false,
         color: '960018'
     },
     skywards_business: {
         text:
             'With Emirates Business Rewards, your organisation earns Points on all your business travel, which you can spend on future flights or upgrades on Emirates. Our rewards are flexible so you can choose any fare type, on any flight, in any class. Subject to availability. The number of Points you’ll need for a reward will depend on seat availability and the fare on that day. Manage all your travel in one place and get the most out of your corporate travel budget with easy reporting on Points earned, rewards booked, top routes and more.',
-        position: '0.2 1.5 -2',
+        position: '0 1.5 -1',
         enabled: false,
-        color: 'd21f3c'
+        color: 'D21f3C'
     },
     item_wifi: {
         text: `Stay connected with family and friends with two hours of free text messaging on WhatsApp, iMessage, Facebook Messenger, Viber Chat or WeChat. Or you can use 20MB of data for free within two hours of login. Simply log in to our OnAir onboard Wi-Fi and choose how you want to connect with your loved ones. You can also choose from our data plans if you would like to check your social media, send emails or browse your favourite sites. And if you're an Emirates Skywards member, you can continue to enjoy free or discounted Wi-Fi throughout your flight depending on your membership tier.`,
-        position: '0.4 1.5 -2',
+        position: '0 1.5 -1',
         enabled: false,
-        color: 'bf0a30'
+        color: 'BF0A30'
     },
     item_dining: {
         text:
             "This January 2020, we're bringing the most delicious plant-based meals to the skies, as we jump on board for Veganuary. Inspired by a blend of cuisines and flavours, our vegan dishes include tasty shitake ravioli, sumptuous bean chilli, and spicy tofu stir fries. Whether you’re a veteran vegan, or just trying something new, you’ll find an option on the menu on all Emirates flights from Dubai to USA, Europe, the UK, Australia, New Zealand, Russia, South Africa, and Addis Ababa. And if you want to carry on after January, we always have a vegan option available if you order in advance.",
-        position: '0.6 1.5 -2',
+        position: '0 1.5 -1',
         enabled: false,
-        color: 'c21807'
+        color: 'C21807'
     },
     entertainment: {
         text:
             "Fly better with over 4,500 channels on our award winning inflight entertainment system ice.Follow your flight's progress on our moving map, and see the world from 40,000ft with our external cameras. Get weather, news and sport reports and information about your flight to your screen. You can listen to your music, radio or podcasts uninterrupted while you explore.",
-        position: '0.8 1.5 -2',
+        position: '0 1.5 -1',
         enabled: false,
-        color: '8d021f'
+        color: '8D021F'
     },
     chauffer: {
         text:
             'Enjoy our complimentary Chauffeur-drive service in over 75 cities worldwide – simply book online through Manage your booking at least 12 hours before your flight. We’ll collect you from your door and drive you to the airport. When you land, we’ll be there to drive you to your final destination – whether that’s to your home, office or to your favourite restaurant.\n',
-        position: '-0.8 1.5 -2',
+        position: '0 1.5 -1',
         enabled: false,
-        color: 'ca3433'
+        color: 'CA3433'
     }
 };
 
@@ -178,13 +178,13 @@ for (let tool in tooltips) {
     $('#main-scene').append(
         $('<a-sphere class="tooltip"></a-sphere').attr({
             id: tool,
-            color: tooltips[tool].position.color,
-            radius: '0.05',
+            color: '#' + tooltips[tool].position.color,
+            radius: '0.001',
             opacity: '1',
-            position: tooltips[tool].position,
+            position: '0.1 1.5 -0.8',
             side: 'double',
             visible: false,
-            text: `align: center; font: exo2bold; color: black; baseline: bottom; width: 1.5; opacity: 0; value: ${tooltips[tool].text}`
+            text: `align: center; font: exo2bold; color: black; baseline: bottom; width: 0.6; opacity: 0; value: ${tooltips[tool].text}`
         })
     );
     $('#' + tool)[0].addEventListener('click', (evt) => {
