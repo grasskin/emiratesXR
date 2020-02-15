e69caaa5d59aae60fe013aa067d2973baeeded8e
Gabriel Rasskin <43894452+grasskin@users.noreply.github.com>
2020-02-14 22:10:08 -0500

    dono

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const serverport = 3000;
+const appport = 6000;
+
+const app = express();
+
+app.use("vr", express.static('public'));
+
+app.use("app", express.static('public'));
+
+app.get('/api/test', (req, res) => {
+    res.send('Hi this is working.');
+});
+
+/*
+app.get('/api/brainwaves', (req, res) => {
+    var now = new Date();
+    if (now - lastFocus < 5000) {
+        data.focusLastFiveSeconds = 1;
+    } else {
+        data.focusLastFiveSeconds = 0;
+    }
+    res.json(data);
+});
+*/
+
+app.listen(serverport, function() {
+    console.log('server listening on port ' + serverport);
+});
