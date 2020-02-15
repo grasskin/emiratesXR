3f2a9491a529f8fb56d397ba85d446b3a6845aed
Gabriel Rasskin <43894452+grasskin@users.noreply.github.com>
2020-02-14 23:28:17 -0500

    app start ;

--- app-public/main.js
+++ app-public/main.js
@@ -1,4 +1,4 @@
-$('#circle').click(function() {
+$('#start').click(function() {
     $.post('/api', { name: 'John', time: '2pm' }).done(function(data) {
         console.log('Data Loaded: ' + data);
     });
