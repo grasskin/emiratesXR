26a3fca0dc5bad49113b768100aba3d2365d7479
navidmx <navidmx@gmail.com>
2020-02-15 12:34:02 -0500

    Scene changes

--- vr-public/script.js
+++ vr-public/script.js
@@ -8,12 +8,52 @@ let pullData = () => {
     });
 }
 
+$.post('/api', { state: 'lobby' }).done((d) => { setInterval(pullData, 500); });
+
+let blackout = $('#blackout')[0];
+
+let fadeOut = (dur) => {
+    blackout.setAttribute(
+        'animation', {
+            property: 'material.opacity',
+            to: 1,
+            dur: dur,
+            easing: 'easeInQuad'
+        },
+        dur);
+    setTimeout(() => { blackout.setAttribute('visible', true); }, dur);
+}
+
+let fadeIn = (dur) => {
+    blackout.setAttribute(
+        'animation', {
+            property: 'material.opacity',
+            to: 0,
+            dur: dur,
+            easing: 'easeInQuad'
+        },
+        dur);
+    setTimeout(() => { blackout.setAttribute('visible', false); }, dur);
+}
+
+let transition = (dur) => {
+    fadeOut(dur / 3);
+    setTimeout(() => { fadeIn(dur / 3); }, dur * (2/3));
+}
+
 let changeState = (state) => {
+    console.log(state);
     switch (state) {
         case 'lobby':
+            $('.environmentGround')[0].setAttribute('visible', true);
+            $('#logo')[0].setAttribute('visible', true)
+            break;
+        case 'start':
+            transition(4000);
+            setTimeout(() => {
+                $('.environmentGround')[0].setAttribute('visible', false);
+                $('#logo')[0].setAttribute('visible', false)
+            }, 2000);
             break;
     }
 }
-
-setInterval(pullData, 500);
-setInterval(compareState, 1000);
